ba4abdc42d72d26e8e26204b1ef7ad4307cd3d86
Rosen Toshev <rtoscheff@gmail.com>
2023-12-14 01:12:44 +0000

    first commit

---
+++ blocking.ts
@@ -0,0 +1,9 @@
+// blocking
+const fs = require("fs/promises");
+const path = require("path");
+const read = async () => {
+  const result = fs.readFile(path.join(__dirname, "package.json"));
+  return result;
+};
+read().then((f) => console.log(f));
+console.log("hi");
---
+++ src/__test__/routes.test.ts
@@ -0,0 +1,18 @@
+import app from "../server";
+import supertest from "supertest";
+
+// describe("GET /", () => {
+//   it("should send back some data", async () => {
+//     const res = await supertest(app).get("/");
+
+//     expect(res.body.message).toBe("hello");
+//   });
+// });
+
+describe("GET /", () => {
+  it("should send back some data", async () => {
+    const res = await supertest(app).get("/");
+
+    expect(res.body.message).toBe("oops, thats on us");
+  });
+});
